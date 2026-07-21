9dde70ff401a6f6b927765abb64e08cb616e6cc3
Marcos Barillas <mbarillas-2023178@kinal.edu.gt>
2026-07-21 12:02:07 -0600

    Se agrego la funcion para conectar la base de datos de Neatbens a Mysql

--- Examen/2023178-3b-exam/src/com/marcosbarillas/conexiondb/ConexionDataBase.java
+++ Examen/2023178-3b-exam/src/com/marcosbarillas/conexiondb/ConexionDataBase.java
@@ -4,10 +4,45 @@
  */
 package com.marcosbarillas.conexiondb;
 
-/**
- *
- * @author Marcos Barillas
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionDataBase {
+
+    private static ConexionDataBase instanciaConexionDatabase;
+    private String usuario = "IN4AM";
+    private String clave = "@dmin4AM"; // usuario de compu de kinal
+    private String clave2 = "in4am"; // Mi compu personal
+    private String url = "jdbc:mysql://localhost:3306/gestor_usuarios_in4av";
+    private final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private Connection instanciaConnection;
+
+    //Ocultar el constructor vacio
+    private ConexionDataBase() {
+        try {
+            Class.forName(DRIVER);
+            
+            //Crear la conexion a la DB
+            instanciaConnection = DriverManager.getConnection(url, usuario , clave2 );
+            
+        } catch( ClassNotFoundException errorClassNotFound){
+            System.out.println("Error de clase no encontrada");
+        } catch( SQLException errorSQL){
+            System.out.println("Error de SQL");
+        } catch (Exception errorPadre){
+            System.out.println("Error top");
+        }
+    }
     
+    public Connection getInstanciaConnection(){
+        return instanciaConnection;
+    }
+
+    public static ConexionDataBase getInstanciaConexionDatabase() {
+        if (instanciaConexionDatabase == null) {
+            instanciaConexionDatabase = new ConexionDataBase();
+        }
+        return instanciaConexionDatabase;
+    }
 }
